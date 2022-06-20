0c15c95ab9f4a9940a18d663af97d68c5ab0f181
developer-yopi <developer.yopi@gmail.com>
2022-06-20 11:30:26 +0900

    コード記入関数を追加

--- generate_score.py
+++ generate_score.py
@@ -19,40 +19,49 @@ def main():
 
     # ファイル名のリストを取得
     file_list = os.listdir()
-    for x in file_list:
-        if x.endswith('.mscz'):
-            mscz = x
+    for file in file_list:
+        if file.endswith('.mscz'):
+            mscz = file
             break
 
     title = mscz.replace('.mscz', '')
-    tmp = title + '/tmp'
-    subprocess.run(['mkdir', '-p', tmp])
+    tmp_dir = './tmp/'
+    # subprocess.run(['mkdir', '-p', tmp_dir])
+    subprocess.run(['mkdir', tmp_dir])
 
     # msczをunzipし、mscxをtmpディレクトリに格納
     with ZipFile(mscz, 'r') as mscx_zip:
         file_names = mscx_zip.namelist()
+        print(file_names)
         for file_name in file_names:
+            mscx_zip.extract(file_name, tmp_dir)
             if file_name.endswith('.mscx'):
-                mscx_zip.extract(file_name, tmp)
-                break
-    # mscx = tmp + '/' + title + '.mscx'
+                mscx_zip.extract(file_name, tmp_dir)
+                mscx = tmp_dir + title + '.mscx'
+                subprocess.run(['mv', tmp_dir + file_name, mscx])
+                # break
 
     # mscxをparse
-    tree = etree.parse(tmp + '/' + title + '.mscx')
+    tree = etree.parse(mscx)
     root = tree.getroot()   
 
     staffs = tree.xpath('//Staff')
-    for staff in staffs[len(staffs)//2:]:
+    for i, staff in enumerate(staffs[len(staffs)//2:]):
         for voice in staff.xpath('./Measure/voice'):
             for rest in voice.xpath('./Rest'):
-                chord = new_chord()
+                if i == 0:  # 一番上の段にのみコード情報を入れる
+                    root, name, base = separate_chord()
+                    voice.append(new_harmony(root=root, name=name, base=base))
+                # 休符を消し音符を入力
                 voice.remove(rest)
-                voice.append(chord)
+                voice.append(new_chord())
 
     # print(etree.tostring(tree).decode())
 
+    # ファイル出力
     tree.write(
-        'a.mscx',
+        # mscx,
+        './a.mscx',
         pretty_print = True,
         xml_declaration = True,
         encoding = 'utf-8'
@@ -61,32 +70,75 @@ def main():
 
 ### 関数 ###
 
-# 新しくchord要素を作る
+# 新しいchord要素
 def new_chord():
     # chord要素の準備
-    chord = etree.Element('Chord')
-    durationType = etree.Element('durationType')
-    note = etree.Element('Note')
-    pitch = etree.Element('pitch')
-    tpc = etree.Element('tpc')
-
-    # 親子関係の構築
-    chord.append(durationType)
-    chord.append(note)
-    note.append(pitch)
-    note.append(tpc)
+    chord_elem = etree.Element('Chord')
+    duration_type_elem = etree.Element('durationType')
+    note_elem = etree.Element('Note')
+    pitch_elem = etree.Element('pitch')
+    tpc_elem = etree.Element('tpc')
 
     # 要素に内容を書き込む
-    durationType.text = 'whole'
-    pitch.text = '60'
-    tpc.text = '14'
+    duration_type_elem.text = 'whole'   # 全音符
+    pitch_elem.text = '60'              # C4
+    tpc_elem.text = '14'                # C
 
-    return chord
+    # 親子関係の構築
+    chord_elem.append(duration_type_elem)
+    chord_elem.append(note_elem)
+    note_elem.append(pitch_elem)
+    note_elem.append(tpc_elem)
 
-# パート別に適切なpitchとtpcを選ぶ関数
-def choose_pitch():
-    return
+    return chord_elem
 
+# 新しいharmony要素
+def new_harmony(root:int=14, name:str=None, base:int=None):
+    # harmony要素の準備
+    harmony_elem = etree.Element('Harmony')
+    root_elem = etree.Element('root')
+    root_case_elem = etree.Element('rootCase')
+    name_elem = etree.Element('name')
+    base_elem = etree.Element('base')
+    base_case_elem = etree.Element('baseCase')
+
+    # 要素に内容を書き込む
+    root_elem.text = str(root)
+    root_case_elem.text = '1'
+    name_elem.text = name
+    base_elem.text = str(base)
+    base_case_elem.text = '1'
+
+    # 親子関係の構築
+    harmony_elem.append(root_elem)
+    harmony_elem.append(root_case_elem)
+    if name:  # コード名が存在する場合
+        harmony_elem.append(name_elem)
+    if base != None:  # 基音が根音と異なる場合
+        harmony_elem.append(base_elem)
+        harmony_elem.append(base_case_elem)
+    return harmony_elem
+
+# 音名から Tonal Pitch Class を取得する
+def get_tpc(note: str='C'):
+    return 14
+
+# コードのテキストを root, name, base に分ける
+def separate_chord(chord: str='Cmaj7'):
+    # 根音はコード名の先頭の文字
+    root = get_tpc(note=chord[0])
+    # 分数コードはコード名と基音を分ける
+    if '/' in chord:
+        name = chord[1:-1]
+        base = get_tpc(chord[-1])
+    elif 'on' in chord:
+        name = chord[1:-2]
+        base = get_tpc(chord[-1])
+    # 分数コードでない場合、基音は None
+    else:
+        name = chord[1:]
+        base = None
+    return root, name, base
 
 
 if __name__ == "__main__":
